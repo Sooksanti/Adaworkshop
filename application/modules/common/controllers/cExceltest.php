9c438e6839986a577b1a9c36dc2bd330bddbd28d
Sooksanti <sooksanti.t@gmail.com>
2020-07-31 18:20:19 +0700

    add border

--- application/modules/common/controllers/cExceltest.php
+++ application/modules/common/controllers/cExceltest.php
@@ -6,6 +6,9 @@ use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
 use Box\Spout\Common\Entity\Row;
 use Box\Spout\Common\Entity\Style\Color;
 use Box\Spout\Writer\Common\Creator\Style\StyleBuilder;
+use Box\Spout\Common\Entity\Style\CellAlignment;
+use Box\Spout\Common\Entity\Style\Border;
+use Box\Spout\Writer\Common\Creator\Style\BorderBuilder;
 
 class cExceltest extends MX_Controller {
     public function __construct() {
@@ -14,21 +17,35 @@ class cExceltest extends MX_Controller {
     }
 	public function test()
 	{	        
-
         $writer = WriterEntityFactory::createXLSXWriter();
         // $writer = WriterEntityFactory::createODSWriter();
         // $writer = WriterEntityFactory::createCSVWriter();
         $writer->setShouldUseInlineStrings(true); // default (and recommended) value
     
         $writer->setTempFolder($customTempFolderPath);
-    
+
+        $border = (new BorderBuilder())
+        ->setBorderBottom(Color::BLACK, Border::WIDTH_THIN, Border::STYLE_SOLID)
+        ->build();
+
+                /** Create a style with the StyleBuilder */
+                $style = (new StyleBuilder())
+                ->setFontBold()
+                ->setFontSize(14)
+                ->setFontColor(Color::BLACK)
+                ->setShouldWrapText()
+                ->setBorder($border)
+                ->setCellAlignment(CellAlignment::RIGHT)
+                ->setBackgroundColor(Color::YELLOW)
+                ->build();
+
         $fileName = 'test.xlsx';
     
         // $writer->openToFile($filePath); // write data to a file or to a PHP stream
         $writer->openToBrowser($fileName); // stream data directly to the browser
         $cells = [
-            WriterEntityFactory::createCell('ID'),
-            WriterEntityFactory::createCell('Country'),
+            WriterEntityFactory::createCell('ID',$style),
+            WriterEntityFactory::createCell('Country',$style),
         ];
     
         /** add a row at a time */
